aee8d0a384f1bf6adb11355f4933bf9441ae7f01
shubhamsugara22 <shubhamsugara22@gmail.com>
2024-12-30 23:48:27 +0530

    Add button using stadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
       <main className="">
         <h1>Lets build AI productivity app</h1>
+
+        <Button>Clcik me</Button>
       </main>
     );
 }
