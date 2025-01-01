5d91a967aeaf7bf6945ec1f79132dc3cc020c971
shubhamsugara22 <shubhamsugara22@gmail.com>
2025-01-01 21:46:51 +0530

    Start header skeleton in main page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Header from "@/components/ui/Header";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,7 +16,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}</body>
     </html>
     </ClerkProvider>
   );
