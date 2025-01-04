ff17e5648a497576ac7ca91e17fdf6773d1a9d0d
shubhamsugara22 <shubhamsugara22@gmail.com>
2025-01-04 23:05:48 +0530

    Added shadcn sheet and drawer to sidecar

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -1,8 +1,32 @@
 import NewDocumentButton from './NewDocumentButton';
+import {
+	Sheet,
+	SheetContent,
+	SheetDescription,
+	SheetHeader,
+	SheetTitle,
+	SheetTrigger,
+  } from "@/components/ui/sheet"
+  
+  
 function Sidebar() {
   return (
 	<div className='p-2 md:p-5 bg-gray-200 relative'>
-		<NewDocumentButton />
+		<Sheet>
+            <SheetTrigger>Open</SheetTrigger>
+            <SheetContent side="left">
+                <SheetHeader>
+                    <SheetTitle>Menu</SheetTitle>
+                    <SheetDescription>
+                        This action cannot be undone. This will permanently delete your account
+                        and remove your data from our servers.
+                    </SheetDescription>
+                </SheetHeader>
+            </SheetContent>
+        </Sheet>
+		<div className="hidden md:inline">
+		    <NewDocumentButton />
+		</div>
 
 
 	</div>
