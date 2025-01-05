ed4241ea31ede3a5c80f4dc6949928cd5b718b1f
shubhamsugara22 <shubhamsugara22@gmail.com>
2025-01-05 22:39:00 +0530

    Fix the sidecar and add button

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { MenuIcon } from 'lucide-react';
 import NewDocumentButton from './NewDocumentButton';
 import {
 	Sheet,
@@ -10,22 +11,32 @@ import {
   
   
 function Sidebar() {
+    const menuOptions = (
+      <>
+        <NewDocumentButton />
+        {/* My Documents */}
+        {/* List ...*/}
+        {/* Shared with me*/}
+        {/* List ...*/}
+      </>
+    );
   return (
 	<div className='p-2 md:p-5 bg-gray-200 relative'>
+    <div className= "md:hidden">
 		<Sheet>
-            <SheetTrigger>Open</SheetTrigger>
+            <SheetTrigger>
+              <MenuIcon className="p-2 hover:opacity-30 rounded-lg " size={40} />
+            </SheetTrigger>
             <SheetContent side="left">
                 <SheetHeader>
                     <SheetTitle>Menu</SheetTitle>
-                    <SheetDescription>
-                        This action cannot be undone. This will permanently delete your account
-                        and remove your data from our servers.
-                    </SheetDescription>
+                    <div>{menuOptions}</div>
                 </SheetHeader>
             </SheetContent>
         </Sheet>
+        </div>  
 		<div className="hidden md:inline">
-		    <NewDocumentButton />
+      {menuOptions}
 		</div>
 
 
