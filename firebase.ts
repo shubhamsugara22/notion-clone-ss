56b2daa06eebfb8b391863a0c0685e8a0cfe9cfc
shubhamsugara22 <shubhamsugara22@gmail.com>
2025-01-08 21:28:28 +0530

    update firebase package

--- firebase.ts
+++ firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+//import { getAnalytics } from "firebase/analytics";
 import { getFireStore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,5 +18,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) :getApp();
+const db = getFireStore(app);
+
+export { db };
