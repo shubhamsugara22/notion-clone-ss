a688331d1cd69886301b647b0b3a4d79de9ea5d7
shubhamsugara22 <shubhamsugara22@gmail.com>
2025-01-13 21:20:52 +0530

    update page.tsx with new style

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { ArrowLeftCircle } from "lucide-react";
 
 export default function Home() {
   return (
-      <main className="">
-        <h1>Lets build AI productivity app</h1>
+      <main className="flex space-x-2 items-center animate-pulse">
+        <ArrowLeftCircle className="w-12 h-12" />
+        <h1 className="font-bold">Get started with creating a new document</h1>
 
         <Button>Click me</Button>
       </main>
