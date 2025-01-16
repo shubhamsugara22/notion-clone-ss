dc507c3dc739c2fd9e3f3cfabee428e962d61bad
shubhamsugara22 <shubhamsugara22@gmail.com>
2025-01-16 23:19:40 +0530

    Added breadcrump component

--- components/Breadcrumbs.tsx
+++ components/Breadcrumbs.tsx
@@ -1,14 +1,40 @@
 'use client'
 
 import { usePathname } from "next/navigation"
-
+import {
+	Breadcrumb,
+	BreadcrumbItem,
+	BreadcrumbLink,
+	BreadcrumbList,
+	BreadcrumbPage,
+	BreadcrumbSeparator,
+  } from "@/components/ui/breadcrumb"
+  
 function Breadcrumbs() {
   const path = usePathname();  
-  
   const segments = path.split("/");
+  
   return (
-	<div>Breadcrumbs</div>
-  )
+	<Breadcrumb>
+        <BreadcrumbList>
+            <BreadcrumbItem>
+                <BreadcrumbLink href="/">Home</BreadcrumbLink>
+            </BreadcrumbItem>
+
+			{segments.map((segment, index) => {
+				if(!segment) return null;
+			   
+			    const href = `/${segments.slice(0, index + 1).join("/")}`;
+
+				return (
+					<BreadcrumbItem key={segment}>
+						<BreadcrumbLink href={href}>{segment}</BreadcrumbLink>
+					</BreadcrumbItem>
+				    );
+			})}
+        </BreadcrumbList>
+    </Breadcrumb>
+  );
 }
 
 export default Breadcrumbs
