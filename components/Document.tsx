b17bf2ec18df8702eb562219aee3e3f81f590a42
shubhamsugara22 <shubhamsugara22@gmail.com>
2025-01-17 22:13:06 +0530

    Add custom hook useOwner - Initial setup

--- components/Document.tsx
+++ components/Document.tsx
@@ -11,6 +11,7 @@ function Document({ id }: { id: string }) {
   const [data, loading, error] = useDocumentData(doc(db, "documents", id));
   const [input, setInput] = useState("");
   const [isUpdating, startTransition] = useTransition();
+  const isOwner = useOwner();
 
   useEffect(() => {
     if (data) {
@@ -49,6 +50,7 @@ function Document({ id }: { id: string }) {
 
       {/* Avatars */}
     </div>
+    {/* Collaborative editor */}
   </div>
   )
 }
