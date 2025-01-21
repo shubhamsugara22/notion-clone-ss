e7d690f1e490dd96535f5fcaf5e01fecb538db96
shubhamsugara22 <shubhamsugara22@gmail.com>
2025-01-21 22:59:44 +0530

    update livecursor component

--- components/LiveCursorProvider.tsx
+++ components/LiveCursorProvider.tsx
@@ -1,12 +1,32 @@
 'use client'
-import { useMyPresence } from "@liveblocoks/react/suspense";
+import { useMyPresence, useOthers } from "@liveblocks/react/suspense";
+import { PointerEvent } from "react";
 function LiveCursorProvider({children} : {children: React.ReactNode}) {
   const [myPresence , updateMyPresence] = useMyPresence();
   const others = useOthers();
   
+  function handlePointerMove(e: PointerEvent<HTMLDivElement>) {
+    // Update from clientx and clienty to pageX and pageY
+    const cursor = {x: Math.floor(e.pageX), y: Math.floor(e.pageY)};
+    updateMyPresence({cursor});
+  }
+  function handlePointerLeave() {
+    updateMyPresence({cursor: null});
+  }  
   return (
-	<div>
-    {/* Render cursor*/}
+	<div
+    onPointerMove={handlePointerMove}
+    onPointerLeave={handlePointerLeave}
+    >
+    {others.filter((other) => other.presence.cursor !== null)
+    .map(({ connectionId, presence, info}) => (
+      <FollowPointer
+      key={connectionId}
+      info={info}
+      x={presence.cursor!.x}
+      y={presence.cursor!.y}
+      />
+    ))}
     </div>
   )
 }
