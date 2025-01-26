54c846634d32aeba74d33c5f6c90b267719d7ccc
shubhamsugara22 <shubhamsugara22@gmail.com>
2025-01-26 23:12:38 +0530

    Add useOwner component

--- components/Document.tsx
+++ components/Document.tsx
@@ -12,7 +12,7 @@ function Document({ id }: { id: string }) {
   const [data, loading] = useDocumentData(doc(db, "documents", id));
   const [input, setInput] = useState("");
   const [isUpdating, startTransition] = useTransition();
-  // const isOwner = useOwner();
+  const isOwner = useOwner();
 
   useEffect(() => {
     if (data) {
@@ -33,7 +33,7 @@ function Document({ id }: { id: string }) {
   };
 
   return (
-	<div>
+	<div className="flex-1 h-full bg-white p-5">
     <div className="flex max-w-6xl mx-auto justify-between pb-5">
       <form className="flex flex-1 space-x-2" onSubmit={updateTitle}>
         {/* update title ... */}
